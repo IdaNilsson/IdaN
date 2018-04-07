1f49a1e179a9bb7a60f501d323e748ad9470baa1
Ida Nilsson <ida__nilsson__@hotmail.com>
2018-04-07 19:05:15 +0200

    Added semicolon

---
+++ queries.sql
@@ -0,0 +1,20 @@
+-- 1
+
+CREATE 
+    ALGORITHM = UNDEFINED 
+    DEFINER = `root`@`localhost` 
+    SQL SECURITY DEFINER
+VIEW `v_allownedmovies` AS
+    SELECT 
+        `movie`.`id` AS `id`,
+        `movie`.`title` AS `title`,
+        `movie`.`genre` AS `genre`,
+        `movie`.`actor` AS `actor`,
+        `movie`.`director` AS `director`,
+        `movie`.`releaseYear` AS `releaseYear`,
+        `movie`.`isLeased` AS `isLeased`
+    FROM
+        `movie`
+
+
+select * from v_allownedmovies;
